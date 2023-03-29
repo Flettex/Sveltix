850d6f3a38909ebe10817a2fb95cc335964255ed
Rashaad Akbar <rushda2000@gmail.com>
2023-03-29 21:41:09 +0530

    Use a store for the count state

--- src/sveltekit_app/src/routes/Counter.svelte
+++ src/sveltekit_app/src/routes/Counter.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { spring } from 'svelte/motion';
-
-	let count = 0;
+	import { count } from '$lib/test';
 
 	const displayed_count = spring();
-	$: displayed_count.set(count);
+	$: displayed_count.set($count);
 	$: offset = modulo($displayed_count, 1);
 
 	function modulo(n: number, m: number) {
@@ -14,7 +13,7 @@
 </script>
 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+	<button on:click={() => (count.update(v => v-1))} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
@@ -27,7 +26,7 @@
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+	<button on:click={() => (count.update(v => v+1))} aria-label="Increase the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
